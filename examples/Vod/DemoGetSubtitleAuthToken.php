78e945734bcf5d9f5ae3f90e3a545b79229a068c
leiyixing <leiyixing@bytedance.com>
2021-06-24 11:42:56 +0800

    fix: fix bugs

--- examples/Vod/DemoGetSubtitleAuthToken.php
+++ examples/Vod/DemoGetSubtitleAuthToken.php
@@ -11,34 +11,12 @@ $client = Vod::getInstance();
 // $client->setSecretKey("");
 
 $vid = "vid";
-$fileIds = "fileId1,fileId2";
-$formats = "format1,format2";
-$languages = "l1,l2";
-$languageIds = "lId1,lId2";
-$subtitleIds = "s1,s2";
-$status = "status1,status2";
-$title = "title";
-$tags = "tag";
-$ssl = "ssl";
-$offset = "offset";
-$pageSize = "pageSize";
 
 echo "\n获取字幕token\n";
 
 $req = new VodGetSubtitleInfoListRequest();
 
 $req->setVid($vid);
-$req->setFileIds($fileIds);
-$req->setFormats($formats);
-$req->setLanguages($languages);
-$req->setLanguageIds($languageIds);
-$req->setSubtitleIds($subtitleIds);
-$req->setStatus($status);
-$req->setTitle($title);
-$req->setTag($tags);
-$req->setSsl($ssl);
-$req->setOffset($offset);
-$req->setPageSize($pageSize);
 
 try {
     $token = $client->getSubtitleAuthToken($req, 60);
