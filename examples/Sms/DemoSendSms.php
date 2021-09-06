7b4c2dae828c6f3001b6227092b5651eaf078db0
liangyuntao <liangyuntao@bytedance.com>
2021-09-06 11:54:09 +0800

    sms example set region

--- examples/Sms/DemoSendSms.php
+++ examples/Sms/DemoSendSms.php
@@ -4,7 +4,7 @@ use Volc\Service\Sms;
 use Volc\Models\Vod\Request\VodApplyUploadInfoRequest;
 use Volc\Models\Vod\Response\VodApplyUploadInfoResponse;
 
-$client = Sms::getInstance('ap-singapore-1');
+$client = Sms::getInstance('your region');// 不传默认cn-north-1，可选ap-singapore-1 新加坡
 $client->setAccessKey("your ak");
 $client->setSecretKey("your sk");
 
