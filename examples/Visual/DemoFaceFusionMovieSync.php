98ee90bb8ffd6d9c35deee103cd87dcae9a3eae2
刘智会 <liuzhihui.0911@bytedance.com>
2023-04-13 14:15:00 +0000

    Visual ability update0408
    * update visual ability
    * update visual ability
    
    https://code.byted.org/volcengine/volc-sdk-php/merge_requests/281

---
+++ examples/Visual/DemoFaceFusionMovieSync.php
@@ -0,0 +1,21 @@
+<?php
+require('../../vendor/autoload.php');
+use Volc\Service\Visual;
+
+$client = Visual::getInstance();
+// call below method if you dont set ak and sk in ～/.volc/config
+
+$client->setAccessKey($ak);
+$client->setSecretKey($sk);
+
+echo "\nDemo FaceFusionMovieSync\n";
+
+$body = [
+    'req_key' => 'facefusionmovie_standard',
+    // 'binary_data_base64' => ["xxxx"],
+    'image_url' => 'http://xxxx',
+    'video_url' => 'http://xxxx',
+    'enable_face_beautify' => true,
+];
+$response = $client->FaceFusionMovieSync(['json' => $body]);
+echo $response;
---
+++ examples/Visual/DemoT2ILDM.php
@@ -0,0 +1,19 @@
+<?php
+require('../../vendor/autoload.php');
+use Volc\Service\Visual;
+
+$client = Visual::getInstance();
+// call below method if you dont set ak and sk in ～/.volc/config
+
+$client->setAccessKey($ak);
+$client->setSecretKey($sk);
+
+echo "\nDemo T2ILDM\n";
+
+$body = [
+    'req_key' => 't2i_ldm',
+    'text' => 'xxxx',
+    'style_term' => 'cartoon style',
+];
+$response = $client->T2ILDM(['json' => $body]);
+echo $response;
